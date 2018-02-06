b49a4cd948c297283733830f9fb162509a5bece9
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2018-02-05 23:25:50 -0600

    Se actualiza archivos pendientes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,5 @@
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
-<<<<<<< HEAD
 import autoprefixer from 'gulp-autoprefixer';
 import pug from 'gulp-pug';
 import sass from 'gulp-sass';
@@ -39,50 +38,6 @@ const paths = {
 };
 
 // Compiling pug to html
-=======
-import pug from 'gulp-pug';
-import sass from 'gulp-sass';
-import postcss from 'gulp-postcss';
-import cssmin from 'cssnano';
-import babel from 'gulp-babel';
-import jsmin from 'gulp-uglify';
-import sourcemaps from 'gulp-sourcemaps';
-import rename from 'gulp-rename';
-import browserSync from 'browser-sync';
-const server = browserSync.create();
-
-const paths = {
-  pug : {
-    src : 'dev/pug/*.pug',
-    dest : 'app',
-    watch : 'dev/pug/**/*.pug'
-  },
-  styles : {
-    src : 'dev/scss/*.scss',
-    dest : 'app/styles',
-    watch : 'dev/scss/**/*scss'
-  },
-  scripts : {
-    src : 'dev/scripts/*.js',
-    dest : 'app/js',
-    watch : 'dev/scripts/**/*.js'
-  }
-};
-
-const postcssPlugins = [
-  cssmin({
-    core: false,
-    autoprefixer : {
-      add : true,
-      browsers : '> 1%, last 2 versions, Firefox ESR, Opera 12.1'
-    }
-  })
-];
-
-const sassOptions = { outputStyle: 'compact' };
-
-
->>>>>>> 3ad3f667f0e178ba7c2b27091618f99ddbe1ef8e
 gulp.task('pug', () =>
   gulp.src(paths.pug.src)
     .pipe(plumber())
@@ -91,7 +46,6 @@ gulp.task('pug', () =>
     .pipe(server.stream())
 );
 
-<<<<<<< HEAD
 // Compiling sass to css
 gulp.task('styles', () =>
   gulp.src(paths.styles.src)
@@ -110,21 +64,6 @@ gulp.task('styles', () =>
 
 // Compiling JS files
 gulp.task('scripts', () => 
-=======
-gulp.task('styles', () =>
-  gulp.src(paths.styles.src)
-    .pipe(sourcemaps.init({ loadMaps : true }))
-    .pipe(plumber())
-    .pipe(sass(sassOptions))
-    .pipe(postcss(postcssPlugins))
-    .pipe(sourcemaps.write('.'))
-    .pipe(rename('styles.css'))
-    .pipe(gulp.dest(paths.styles.dest))
-    .pipe(server.stream({match : '**/*.css' }))
-);
-
-gulp.task('scripts', () =>
->>>>>>> 3ad3f667f0e178ba7c2b27091618f99ddbe1ef8e
   gulp.src(paths.scripts.src)
     .pipe(babel())
     .pipe(jsmin())
@@ -133,7 +72,6 @@ gulp.task('scripts', () =>
     .pipe(server.stream())
 );
 
-<<<<<<< HEAD
 // Init BrowserSync (Live reload)
 gulp.task('default', ['pug', 'styles', 'scripts'], () => {
   server.init({
@@ -147,16 +85,4 @@ gulp.task('default', ['pug', 'styles', 'scripts'], () => {
   gulp.watch(paths.scripts.look, ['scripts', server.reload]);
   // Watching changes on html files
   // gulp.watch('./app/**/*.html').on('change', server.reload);
-=======
-gulp.task('default', ['pug', 'styles', 'scripts'], () => {
-  server.init({
-    server : {
-      baseDir : './app'
-    }
-  });
-
-  gulp.watch(paths.styles.watch, ['styles']);
-  gulp.watch(paths.pug.watch, ['pug', server.reload]);
-  gulp.watch(paths.scripts.watch, ['scripts', server.reload])
->>>>>>> 3ad3f667f0e178ba7c2b27091618f99ddbe1ef8e
 });
